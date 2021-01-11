ae39fa6aadb2e5276ff86b9ff79149616b147b76
Graidaris <vladislavby96@gmail.com>
2021-01-11 11:23:02 +0300

    update

--- dmpw/db_extractor/db_extractor.py
+++ dmpw/db_extractor/db_extractor.py
@@ -19,7 +19,8 @@ class DB_Extractor:
         self._db = None
         self._cur = None
         self.geometry_column_name = None
-        self.coord_sys_name = None
+        self.native_coord_sys_name = None
+        self.p = None
 
         self.geo_statistic = {}
 
@@ -34,6 +35,17 @@ class DB_Extractor:
             except Exception as e:
                 print(f"EXCEPTION {e}")
     
+    def init_proj(self):
+        try:
+            proj4text = self._db.execute(f'SELECT proj4text FROM geom_cols_ref_sys').fetchone()
+        except sqlite3.OperationalError as e:
+            print(e)
+            return
+
+        self.p = pyproj.Proj(proj4text['proj4text'])
+        self.native_coord_sys_name = proj4text['proj4text'].split(' ')[0].split('=')[1]
+
+
     def set_db_path(self, db_name: str) -> None:
         self.db_path = db_name
 
@@ -58,35 +70,55 @@ class DB_Extractor:
                 for row in geom_result:
                     geom = geojson.loads(row['AsGeoJSON(Geometry)'])
                     geo_objects[column_name].append(geom)
-                    print(geom)
         return geo_objects
-    
+
+    def invers_coord_system(self, x, y):
+        x_lon, y_lat = self.p(x, y, inverse=True)
+        return x_lon, y_lat
+
+    def process_MultiLineString(self, coords):
+        for coords1 in coords:
+            for coords2 in coords1:
+                coords2[0], coords2[1] = self.invers_coord_system(coords2[0], coords2[1])
+
+        return coords
+
+
     def extract_geometry(self) -> tuple:
+        self.init_proj()
         self.get_geometry_statistic()
         geo_objects = self.process_geometry()
 
         geo_linestrings = {}
         geo_poligons = {}
         geo_points = {}
-        
+
         for key in geo_objects.keys():
             array_objects = geo_objects[key]
             for array_object in array_objects:
                 if array_object['type'] == 'LineString' or array_object['type'] == 'MultiLineString':
                     if key in geo_linestrings:
-                        geo_linestrings[key].append(array_object['coordinates'])
+                        geo_linestrings[key][self.native_coord_sys_name].append(array_object['coordinates'])
                     else:
-                        geo_linestrings[key] = [array_object['coordinates']]
+                        geo_linestrings[key] = {
+                            self.native_coord_sys_name: [array_object['coordinates']]
+                            }
+
                 elif array_object['type'] == 'Polygon' or array_object['type'] == 'MultiPolygon':
                     if key in geo_poligons:
-                        geo_poligons[key].append(array_object['coordinates'])
+                        geo_poligons[key][self.native_coord_sys_name].append(array_object['coordinates'])
                     else:
-                        geo_poligons[key] = [array_object['coordinates']]
+                        geo_poligons[key] = {
+                            self.native_coord_sys_name: [array_object['coordinates']]
+                            }
+
                 elif array_object['type'] == 'Point' or array_object['type'] == 'MultiPoint':
                     if key in geo_points:
-                        geo_points[key].append(array_object['coordinates'])
+                        geo_points[key][self.native_coord_sys_name].append(array_object['coordinates'])
                     else:
-                        geo_points[key] = [array_object['coordinates']]
+                        geo_points[key] = {
+                            self.native_coord_sys_name: [array_object['coordinates']]
+                            }
 
         return (geo_linestrings, geo_poligons, geo_points)
 
@@ -94,36 +126,35 @@ class DB_Extractor:
     
     def get_geometry_statistic(self):
         try:
-            proj4text = self._db.execute(f'SELECT proj4text FROM geom_cols_ref_sys').fetchone()
             statistic = self._db.execute(f'SELECT MIN(extent_min_x), MIN(extent_min_y), MAX(extent_max_x), MAX(extent_max_y) FROM geometry_columns_statistics').fetchone()
         except sqlite3.OperationalError as e:
             print(e)
             return
+        
 
-        p = pyproj.Proj(proj4text['proj4text'])
-        coord_sys_name = proj4text['proj4text'].split(' ')[0].split('=')[1]
-        self.coord_sys_name = coord_sys_name
+        if self.native_coord_sys_name == 'merc':
 
-        if coord_sys_name == 'merc':
             self.geo_statistic['MIN_X'] = statistic["MIN(extent_min_x)"]
             self.geo_statistic['MIN_Y'] = statistic["MIN(extent_min_y)"]
             self.geo_statistic['MAX_X'] = statistic["MAX(extent_max_x)"]
             self.geo_statistic['MAX_Y'] = statistic["MAX(extent_max_y)"]
-            lon, lat = p(self.geo_statistic['MIN_X'], self.geo_statistic['MIN_Y'], inverse=True)
+            lon, lat = self.p(self.geo_statistic['MIN_X'], self.geo_statistic['MIN_Y'], inverse=True)
             self.geo_statistic['MIN_LON'] = lon
             self.geo_statistic['MIN_LAT'] = lat
-            lon, lat = p(self.geo_statistic['MAX_X'], self.geo_statistic['MAX_Y'], inverse=True)
+            lon, lat = self.p(self.geo_statistic['MAX_X'], self.geo_statistic['MAX_Y'], inverse=True)
             self.geo_statistic['MAX_LON'] = lon
             self.geo_statistic['MAX_LAT'] = lat
-        elif coord_sys_name == 'longlat':
+
+        elif self.native_coord_sys_name == 'longlat':
+
             self.geo_statistic['MIN_LON'] = statistic["MIN(extent_min_x)"]
             self.geo_statistic['MIN_LAT'] = statistic["MIN(extent_min_y)"]
             self.geo_statistic['MAX_LON'] = statistic["MAX(extent_max_x)"]
             self.geo_statistic['MAX_LAT'] = statistic["MAX(extent_max_y)"]
-            min_x, min_y = p(self.geo_statistic['MIN_LON'], self.geo_statistic['MIN_LAT'], inverse=True)
+            min_x, min_y = self.p(self.geo_statistic['MIN_LON'], self.geo_statistic['MIN_LAT'], inverse=True)
             self.geo_statistic['MIN_X'] = min_x
             self.geo_statistic['MIN_Y'] = min_y
-            max_x, max_y = p(self.geo_statistic['MAX_X'], self.geo_statistic['MAX_Y'], inverse=True)
+            max_x, max_y = self.p(self.geo_statistic['MAX_X'], self.geo_statistic['MAX_Y'], inverse=True)
             self.geo_statistic['MAX_X'] = max_x
             self.geo_statistic['MAX_Y'] = max_y
 
